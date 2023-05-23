36f23cfbbf360d05d0a26d900d03190bba804165
Leandro Arturi <lea.arturi@gmail.com>
2023-05-22 23:12:48 -0300

    Fix Slack Notification

--- middlewares/error-handler.ts
+++ middlewares/error-handler.ts
@@ -18,40 +18,16 @@ export const errorhandler = (
    const dateWithMinutesAndSeconds: string = currentDate();
 
    if (err instanceof CustomError) {
-      console.log({
-         logDate: dateWithMinutesAndSeconds,
-         status: err.statusCode,
-         message: err.message,
-      });
-
-      insertToCSV([
-         dateWithMinutesAndSeconds,
-         'ERROR',
-         `{ status: ${err.statusCode.toString()}, message: ${err.message} }`,
-      ]);
-
-      axios.post(slackUrl, {
-         text: `Error en API Yahoo Finances. ${req.body.text}`,
-      });
-
+      console.log({ logDate: dateWithMinutesAndSeconds, status: err.statusCode, message: err.message });
+      insertToCSV([dateWithMinutesAndSeconds, 'ERROR', `{ status: ${err.statusCode.toString()}, message: ${err.message} }`]);
+      axios.post(slackUrl, { text: `Error en API Yahoo Finances. ${req.body.text}` });
       return res.status(err.statusCode).send({ errors: err.serializeErrors() });
    } else {
-      console.log({
-         logDate: dateWithMinutesAndSeconds,
-         status: 400,
-         message: err.message,
-      });
-
-      insertToCSV([
-         dateWithMinutesAndSeconds,
-         'ERROR',
-         `{ status: 400, message: ${err.message} }`,
-      ]);
+      console.log({ logDate: dateWithMinutesAndSeconds, status: 400, message: err.message });
+      insertToCSV([dateWithMinutesAndSeconds,'ERROR',`{ status: 400, message: ${err.message} }`]);
 
       if (err.message === 'Request failed with status code 401') {
-         axios.post(slackUrl, {
-            text: `Error en API Yahoo Finances. ${err.message}. ${dateWithMinutesAndSeconds}`,
-         });
+         axios.post(slackUrl, { text: `Error en API Yahoo Finances. ${err.message}. ${dateWithMinutesAndSeconds}`});
       }
    }
 
